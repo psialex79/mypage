b16e3e6f6e6f30daf1fa4f08e5346369290b5f1c
psialex79 <psialex79@gmail.com>
2023-12-21 17:02:56 +0300

    настройки Posgtgres

--- mypage/settings.py
+++ mypage/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'mypage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypagedb',
+        'USER': 'buddy ',
+        'PASSWORD': '12211221',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
