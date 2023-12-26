8e4592113e8a98948f324a546026af3698e2d405
psialex79 <psialex79@gmail.com>
2023-12-26 18:11:21 +0300

    Добавлена функция получения курсов

--- currency_tracker/views.py
+++ currency_tracker/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from .models import CurrencyRate
 import json
 
-def index(request):
+def get_currency_data():
     rates = CurrencyRate.objects.all().order_by('date')
     data_labels = [rate.date.strftime("%Y-%m-%d") for rate in rates]
     data_rates = [rate.rate for rate in rates]
+    return data_labels, data_rates
+
+def index(request):
+    data_labels, data_rates = get_currency_data()
     context = {
         'data_labels': json.dumps(data_labels),
         'data_rates': json.dumps(data_rates)
