f4308123675ff0f6b38d41fae1fd429edc381b52
psialex79 <psialex79@gmail.com>
2023-12-27 15:35:39 +0300

    работа с обновлением курсов

--- currency_tracker/currency_updater.py
+++ currency_tracker/currency_updater.py
@@ -15,14 +15,17 @@ def fetch_historical_data():
             print(f'Данные за {start_date} уже есть в базе данных.')
         except ObjectDoesNotExist:
             formatted_date = start_date.strftime('%Y-%m-%d')
-            url = f'http://api.exchangeratesapi.io/v1/{formatted_date}?access_key={api_key}&base=USD&symbols=RUB'
+            url = f'http://api.exchangeratesapi.io/v1/{formatted_date}?access_key={api_key}&symbols=RUB,USD'
             response = requests.get(url)
             if response.status_code == 200:
                 data = response.json()
-                if 'rates' in data and 'RUB' in data['rates']:
-                    rate = CurrencyRate(date=start_date, rate=data['rates']['RUB'])
+                if 'rates' in data and 'RUB' in data['rates'] and 'USD' in data['rates']:
+                    eur_to_rub = data['rates']['RUB']
+                    eur_to_usd = data['rates']['USD']
+                    usd_to_rub = eur_to_rub / eur_to_usd
+                    rate = CurrencyRate(date=start_date, rate=usd_to_rub)
                     rate.save()
-                    print(f'Данные за {formatted_date} добавлены в базу данных.')
+                    print(f'Курс USD/RUB за {formatted_date} добавлен в базу данных.')
                 else:
                     print(f'Нет данных о курсе валют для {formatted_date}')
             else:
