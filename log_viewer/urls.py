b4977991dc9965ef494c7e390e3e533622678ff5
psialex79 <psialex79@gmail.com>
2024-01-14 17:18:14 +0300

    Работа на страницей с логами.

--- log_viewer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('log-viewer', views.log_viewer, name='log_viewer'),
-]
--- log_viewer/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import os
-
-def log_viewer(request):
-    with open('/var/log/eduhelperbot.err.log', 'r') as file:
-        logs = file.read()
-    return render(request, 'log_viewer/log_viewer.html', {'logs': logs})
