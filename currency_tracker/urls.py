25412856bfea1962712c4bbde69c57b34038b744
psialex79 <psialex79@gmail.com>
2024-01-14 16:56:03 +0300

    Работа на страницей с логами.

--- currency_tracker/urls.py
+++ currency_tracker/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('currency-data/', views.currency_data, name='currency_data'),
     path('update-currency-rates/', views.update_currency_rates, name='update_currency_rates'),
-    path('log-viewer/', include('log_viewer.urls')),
 ]
