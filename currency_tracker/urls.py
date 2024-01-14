e04a39938993e1d0058fafa6edf13d0bf0095c9c
psialex79 <psialex79@gmail.com>
2024-01-14 18:16:06 +0300

    Работа на страницей с логами.

--- currency_tracker/urls.py
+++ currency_tracker/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('currency-tracker/currency-data/', views.currency_data, name='currency_data'),
-    path('currency-tracker/update-currency-rates/', views.update_currency_rates, name='update_currency_rates'),
+    path('currency-data/', views.currency_data, name='currency_data'),
+    path('update-currency-rates/', views.update_currency_rates, name='update_currency_rates'),
 ]
