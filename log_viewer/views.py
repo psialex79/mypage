1599613684f0e2b801c1e0d88a485b13e8194fc7
psialex79 <psialex79@gmail.com>
2024-01-14 19:47:21 +0300

    Работа на страницей с логами.

--- log_viewer/views.py
+++ log_viewer/views.py
@@ -3,26 +3,11 @@ from django.http import HttpResponse
 import os
 import re
 
-def filter_russian_text(logs):
-    # Регулярное выражение для поиска русскоязычных фрагментов
-    russian_text_pattern = re.compile(r'[А-Яа-яЁё]+[\w\s]*[А-Яа-яЁё]+')
-
-    filtered_logs = []
-    for line in logs:
-        # Извлекаем русскоязычные фрагменты из строки
-        russian_texts = russian_text_pattern.findall(line)
-        if russian_texts:
-            # Соединяем все найденные фрагменты в одну строку
-            filtered_line = ' '.join(russian_texts)
-            filtered_logs.append(filtered_line)
-    
-    return filtered_logs
 
 def log_viewer(request):
     with open('/var/log/eduhelperbot.err.log', 'r') as file:
         logs = file.readlines() # читаем логи построчно
 
-    filtered_logs = filter_russian_text(logs) # фильтруем логи
-    filtered_logs.reverse() # переворачиваем список, чтобы новые сообщения были в начале
-    return render(request, 'log_viewer/log_viewer.html', {'logs': filtered_logs})
+    logs.reverse() # переворачиваем список, чтобы новые сообщения были в начале
+    return render(request, 'log_viewer/log_viewer.html', {'logs': logs})
 
