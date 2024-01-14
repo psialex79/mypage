ee4cf8a1f92e9403b46de0ddbe37dee3990091d6
psialex79 <psialex79@gmail.com>
2024-01-14 16:47:44 +0300

    Работа на страницей с логами.

--- currency_tracker/urls.py
+++ currency_tracker/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('currency-data/', views.currency_data, name='currency_data'),
     path('update-currency-rates/', views.update_currency_rates, name='update_currency_rates'),
+    path('log-viewer/', include('log_viewer.urls')),
 ]
--- log_viewer/views.py
+++ log_viewer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
-# Create your views here.
+def log_viewer(request):
+    with open('/var/log/eduhelperbot.err.log', 'r') as file:
+        logs = file.read()
+    return render(request, 'log_viewer.html', {'logs': logs})
