bad1072458ffbc2573abd9c881d27ff9b8e09dac
psialex79 <psialex79@gmail.com>
2024-01-29 20:38:24 +0300

    users checker

--- users_checker/admin.py
+++ users_checker/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import Follower
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
     list_display = ['telegram_id', 'date_of_exclusion']
