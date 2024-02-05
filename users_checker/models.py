697454506b70f15f90578db310dbe5982b1d963a
psialex79 <psialex79@gmail.com>
2024-02-05 09:23:02 +0300

    Follower changes

--- users_checker/models.py
+++ users_checker/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Follower(models.Model):
     telegram_id = models.BigIntegerField(unique=True)
     date_of_exclusion = models.DateField()
+    name = models.CharField(max_length=255, blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"User {self.telegram_id}"
+        return f"Follower {self.telegram_id}"
+
