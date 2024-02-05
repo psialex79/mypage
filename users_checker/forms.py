f22375b15069a67cdcb89ec29e4523416792fe22
psialex79 <psialex79@gmail.com>
2024-02-05 09:32:24 +0300

    Follower changes

--- users_checker/forms.py
+++ users_checker/forms.py
@@ -4,4 +4,4 @@ from .models import Follower
 class FollowerForm(forms.ModelForm):
     class Meta:
         model = Follower
-        fields = ['telegram_id', 'date_of_exclusion']
+        fields = ['telegram_id', 'date_of_exclusion', 'name', 'note']
--- users_checker/serializers.py
+++ users_checker/serializers.py
@@ -4,4 +4,4 @@ from .models import Follower
 class FollowerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follower
-        fields = ['telegram_id', 'date_of_exclusion']
+        fields = ['telegram_id', 'date_of_exclusion', 'name', 'note']
