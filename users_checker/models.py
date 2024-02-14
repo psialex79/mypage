0f5ad6f59a564aaeb3934adc88aa1965779ffe32
psialex79 <psialex79@gmail.com>
2024-02-14 09:38:28 +0300

    справление админки бота изипикс

--- users_checker/models.py
+++ users_checker/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 
 class Follower(models.Model):
     telegram_id = models.BigIntegerField(unique=True)
-    date_added = models.DateField(auto_now_add=True)  # Автоматическая дата добавления
+    date_added = models.DateField(auto_now_add=True, default=timezone.now)
     date_of_exclusion = models.DateField(blank=True, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     note = models.TextField(blank=True, null=True)
