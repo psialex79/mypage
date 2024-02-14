0ed9b2330cd9d3926f9834b3654a9adb97a7b03b
psialex79 <psialex79@gmail.com>
2024-02-14 09:36:23 +0300

    справление админки бота изипикс

--- users_checker/forms.py
+++ users_checker/forms.py
@@ -4,4 +4,4 @@ from .models import Follower
 class FollowerForm(forms.ModelForm):
     class Meta:
         model = Follower
-        fields = ['telegram_id', 'date_of_exclusion', 'name', 'note']
+        fields = ['telegram_id', 'name', 'note']  # Убрали 'date_of_exclusion'
--- users_checker/serializers.py
+++ users_checker/serializers.py
@@ -4,4 +4,4 @@ from .models import Follower
 class FollowerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follower
-        fields = ['telegram_id', 'date_of_exclusion', 'name', 'note']
+        fields = ['telegram_id', 'name', 'note']  # Убрали 'date_of_exclusion'
