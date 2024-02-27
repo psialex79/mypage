d8a69ecebe99a6f7f03c625e541b75619d0905cb
psialex79 <psialex79@gmail.com>
2024-02-27 13:57:46 +0300

    btc graph

--- currency_tracker/admin.py
+++ currency_tracker/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import CurrencyRate, CurrencyPurchase
+from .models import CurrencyRate, CurrencyPurchase, BitcoinRate, BitcoinPurchase
 
 @admin.register(CurrencyRate)
 class CurrencyRateAdmin(admin.ModelAdmin):
     list_display = ('date', 'rate')
+    list_filter = ('date',)
+    search_fields = ('date', 'rate')
 
 @admin.register(CurrencyPurchase)
 class CurrencyPurchaseAdmin(admin.ModelAdmin):
-    list_display = ('date', 'amount', 'rate')
+    list_display = ('date', 'amount', 'rate', 'spent_amount')
+    list_filter = ('date',)
+    search_fields = ('date', 'amount')
+
+@admin.register(BitcoinRate)
+class BitcoinRateAdmin(admin.ModelAdmin):
+    list_display = ('date', 'rate')
+    list_filter = ('date',)
+    search_fields = ('date', 'rate')
+
+@admin.register(BitcoinPurchase)
+class BitcoinPurchaseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount', 'rate', 'spent_amount_usd')
+    list_filter = ('date',)
+    search_fields = ('date', 'amount')
+
