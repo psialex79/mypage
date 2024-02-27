c4070c4c04db9015dfda2b747b59aa5d1c20a2f5
psialex79 <psialex79@gmail.com>
2024-02-27 14:17:35 +0300

    btc graph

--- currency_tracker/currency_updater.py
+++ currency_tracker/currency_updater.py
@@ -41,7 +41,7 @@ def fetch_historical_btc_usd_data():
         time.sleep(10)
         try:
             BitcoinRate.objects.get(date=start_date)
-            print(f'Данные за {start_date} уже есть в базе данных.')
+            print(f'Данные по BTC/USD за {start_date} уже есть в базе данных.')
         except ObjectDoesNotExist:
             formatted_date = start_date.strftime('%d-%m-%Y')
             url = f'https://api.coingecko.com/api/v3/coins/bitcoin/history?date={formatted_date}&localization=false'
